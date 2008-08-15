d0b07fa2d5ea905fa2f384462a1c079448f68ffd
astrand <astrand@cendio.se>
2008-08-15 10:15:55 +0000

    Added my copyright to the file that I have modified.

--- xclip.c
+++ xclip.c
@@ -3,6 +3,7 @@
  * 
  *  xclip.c - command line interface to X server selections 
  *  Copyright (C) 2001 Kim Saunders
+ *  Copyright (C) 2007-2008 Peter Åstrand
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
