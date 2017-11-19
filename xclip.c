ea29b6be6b8c4b2f20b523b91de530db7dff88b9
Edward Betts <edward@4angle.com>
2017-11-19 16:19:00 +0000

    Special case for xclip to support --help
    When somebody tries a new tool they're likely to try --help to find how
    to use it.
    
    This commit adds a special case to show the usage information when
    called with --help as the first argument.

--- xclip.c
+++ xclip.c
@@ -69,6 +69,13 @@ doOptMain(int argc, char *argv[])
 {
     /* Initialise resource manager and parse options into database */
     XrmInitialize();
+
+    if (!strcmp(argv[1], "--help"))
+    {
+	prhelp(argv[0]);
+	exit(0);
+    }
+
     XrmParseCommand(&opt_db, opt_tab, sizeof(opt_tab) / sizeof(opt_tab[0]), PACKAGE_NAME, &argc,
 		    argv);
 
