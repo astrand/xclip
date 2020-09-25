84f96a259b639b06325c32eddb40f5e323e25cc2
b9 <hackerb9@member.fsf.org>
2020-09-24 17:02:24 -0700

    Merge tree implementing debug verbosity
    Noteworthy change: verbosity level is now called "xcverb" and is part of
    xclib.c, not xclip.c.
    
    Squashed commit of the following:
    
    commit 4975910658d8e87c076f71d8e4c6c13642c9cd6c
    Author: b9 <hackerb9@member.fsf.org>
    Date:   Thu Sep 24 16:59:11 2020 -0700
    
        Formatting niceties
    
    commit c6fd33005c3f88a9e99d7b5ac89ff510c9efb360
    Author: b9 <hackerb9@member.fsf.org>
    Date:   Thu Sep 24 16:56:18 2020 -0700
    
        Formatting niceties
    
    commit 06e3851af567f0dbf13525dab6563f7a912a4de8
    Author: b9 <hackerb9@member.fsf.org>
    Date:   Thu Sep 24 06:35:11 2020 -0700
    
        ev.type is an int
    
    commit f769480d3897810c45c4fe6c8f280472d5e9086d
    Author: b9 <hackerb9@member.fsf.org>
    Date:   Thu Sep 24 06:34:43 2020 -0700
    
        Add more debugging around INCR response
    
    commit 3c34bb38f5a5d3b290bc761f5e99397902cb9033
    Author: b9 <hackerb9@member.fsf.org>
    Date:   Thu Sep 24 06:19:34 2020 -0700
    
        Improve debugging output for -debug option

--- xclib.c
+++ xclib.c
@@ -35,6 +35,9 @@ int xcverb = OSILENT;
 void 
 xcmemzero(void *ptr, size_t len)
 {
+    if (xcverb >= ODEBUG) {
+	fprintf(stderr, "xclip: debug: Zeroing memory buffer\n");
+    }
     memset_func(ptr, 0, len);
 }
 
@@ -362,15 +365,21 @@ xcin(Display * dpy,
 	if (!(*chunk_size)) {
 	    *chunk_size = XMaxRequestSize(dpy) / 4;
 	}
+	if ( xcverb >= ODEBUG ) {
+	    fprintf(stderr,
+		    "xclib: debug: INCR chunk size is %ld\n", (*chunk_size));
+	}
     }
-    if ( xcverb >= ODEBUG )
-	fprintf(stderr, "xclib: debug: INCR chunk size is %ld\n", (*chunk_size));
 
     switch (*context) {
     case XCLIB_XCIN_NONE:
-	if (evt.type != SelectionRequest)
+	if (evt.type != SelectionRequest) {
+	    if ( xcverb >= ODEBUG ) {
+		fprintf(stderr,
+			"xclib: debug: ignoring event type %d\n", evt.type);
+	    }
 	    return (0);
-
+	}
 	/* set the window and property that is being used */
 	*win = evt.xselectionrequest.requestor;
 	*pty = evt.xselectionrequest.property;
@@ -408,6 +417,8 @@ xcin(Display * dpy,
 	}
 	else {
 	    /* send data all at once (not using INCR) */
+	    if ( xcverb >= ODEBUG )
+		fprintf(stderr, "xclib: debug: Sending data all at once\n");
 	    XChangeProperty(dpy,
 			    *win,
 			    *pty, target, 8, PropModeReplace, (unsigned char *) txt, (int) len);
