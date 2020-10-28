7ea708c037018ac6731c06b4e92c6ab204d2cf99
b9 <hackerb9@member.fsf.org>
2020-10-27 23:43:43 -0700

    Create PDF first time run.

--- autogroff.sh
+++ autogroff.sh
@@ -11,6 +11,7 @@ cleanup() {
 
 for f in *.1; do
     file=${f%.1}
+    groff -Tpdf -P-e -P-pletter -mpdfmark -mandoc $file.1 > $file.pdf
     echo "Waiting on $file.1.		HIT ^C TO CANCEL"
     while inotifywait -e MODIFY $file.1; do
 	groff -Tpdf -P-e -P-pletter -mpdfmark -mandoc $file.1 > $file.pdf
